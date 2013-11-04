0f86eb6f781f3fa7d49310af3c8685b196a5d3de
Eun Ji Kim <eunji.june.kim@gmail.com>
2013-11-04 15:29:18 -0500

    commit

--- add_node_name.rb
+++ add_node_name.rb
@@ -5,9 +5,11 @@ column_num = ARGV[2]
 input = input_file.split(".")[0]
 column = column_num.to_i - 1
 
+first_line = open(input_file).gets
+first_entry = first_line.split("\t")[0].tr('"','')
+
 File.open(input_file, "r+") do |file|
 	out = ""
-	first_entry = "Source Name"
 	file.each do |line| 
 		first_item = line.split("\t")[0].tr('"', '')
 		if first_item == first_entry 
@@ -15,9 +17,9 @@ File.open(input_file, "r+") do |file|
 		else
 			column = column_num.to_i - 1
 			node_name = line.split("\t")[column].tr('"', '')
-			line << line.gsub!(node_name, "node_name")
 			new_node_name = string_to_add + " " + node_name
-			out << line.gsub!("node_name", new_node_name)
+			line[line.split("\t")[column]] = new_node_name
+			out << line
 		end
 	end
 	file.pos = 0
